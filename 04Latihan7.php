21ee94fbc71e378b041d10591d0b569cb76cb2c0
muhAgusMaulana <muhammadagusmaulana2@gmail.com>
2020-03-22 22:19:57 +0700

    add file 04Latihan5.php, 04Latihan6.php, 04Latihan7.php, 04Latihan8.php, 04Latihan9.php

---
+++ 04Latihan7.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?Php
+    for ($bil = 1; $bil<25; $bil++)
+        print("$bil<br>\n");
+    ?> 
+</body>
+</html>
